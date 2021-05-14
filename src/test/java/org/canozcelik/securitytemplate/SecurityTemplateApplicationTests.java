5bf029ba59cd6ec3ec6d459b686c7a3a14c18b3a
Can <canozcelik@live.com>
2021-05-14 21:07:18 +0300

    initial commit

---
+++ src/test/java/org/canozcelik/securitytemplate/SecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.canozcelik.securitytemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
